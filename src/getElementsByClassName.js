8037c5b74b6c66cf72b4fe1199a2badb86aba5b3
Hack Reactor Student <student@hackreactor.com>
2017-11-27 12:30:42 -0800

    'Most basic model implementation'

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,15 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, currentElement) {
+  var htmlElement = currentElement;
+  if (htmlElement == null) {
+    htmlElement = document.body;
+  }
+  var relevantElements = [];
+  if (htmlElement.classList.contains(className)) {
+    relevantElements.push(htmlElement); 
+  }
+  return relevantElements;
+ 
 };
