31236e5cbeadc09dac9c2092b81d132d9c7c1397
Hack Reactor Student <student@hackreactor.com>
2017-11-27 12:14:47 -0800

    'Implemented stringifyJSON for all tests'

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,41 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (obj === undefined || typeof obj === 'function' ) {
+    return undefined;
+  }
+  
+  if (typeof obj === 'string') {
+    return '"' + obj + '"'; 
+  }
+
+  if (Array.isArray(obj)) {
+    var stringifiedResults = [];
+    for (var i = 0; i < obj.length; i++) {
+      var stringifiedValue = stringifyJSON(obj[i]);
+      stringifiedResults.push(stringifiedValue);
+    }
+    return '[' + stringifiedResults.join(',') + ']';
+  }
+
+  if (typeof obj === 'object' && obj) {
+    var stringifiedObjectArray = [];
+    for (var key in obj) {
+    
+      var stringifiedKey = stringifyJSON(key);
+      var stringifiedValue = stringifyJSON(obj[key]);
+      if (stringifiedKey !== undefined && stringifiedValue !== undefined ) {
+        var stringifiedProperty = stringifiedKey + ':' + stringifiedValue;
+        stringifiedObjectArray.push(stringifiedProperty);
+      }
+      
+    }
+    return '{' + stringifiedObjectArray.join(',') + '}';
+  }
+
+
+  return '' + obj;
 };
+
+
+
